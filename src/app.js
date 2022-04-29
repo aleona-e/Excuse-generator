07ee690d3b06e80bcc0b17d3435a9b30111f4c67
aleona-e <ale.elsin92@gmail.com>
2022-04-29 16:22:09 +0000

    adding files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "Her bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the code", "my computer", "the car", "the keys"];
+  let when = [
+    "before the class",
+    "while I was cooking",
+    "during lunch",
+    "when I went to bed"
+  ];
+
+  let randomNum1 = Math.random() * who.length;
+  let randomWho = Math.floor(randomNum1);
+  let randomNum2 = Math.random() * action.length;
+  let randomAction = Math.floor(randomNum2);
+  let randomNum3 = Math.random() * what.length;
+  let randomWhat = Math.floor(randomNum3);
+  let randomNum4 = Math.random() * when.length;
+  let randomWhen = Math.floor(randomNum4);
+
+  let excuse =
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
